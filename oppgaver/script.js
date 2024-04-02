9fac9dfc90c1b12db417dc19aeeac0f9a7ff9474
Mohit Verma <mkv97.mkv@gmail.com>
2024-04-02 16:04:32 +0200

    test

--- oppgaver/script.js
+++ oppgaver/script.js
@@ -1,17 +1,29 @@
 const input = document.querySelector('#input')
 const button = document.querySelector('button')
 
-let array = []
+let array = [];
+let local; 
 
 
-const grabValue =  () => {
+const ArrayValue =  () => {
 const userInput = input.value
 array.push(userInput)
 console.log(array)
 }
 
+const arrayToLocal = () => {
+    let local = localStorage.setItem('savedArray', JSON.stringify(array))
+    localToDisplay(local)
+}
+
+const localToDisplay = (local) => {
+    
+}
+
 
 button.addEventListener('click', grabValue)
+
+
 
 /*
 const pushAndSave = () => {
