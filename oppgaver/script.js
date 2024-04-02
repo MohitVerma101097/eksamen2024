78039aed87b88bbc71ac81439529cd275eae201e
Mohit Verma <mkv97.mkv@gmail.com>
2024-04-02 17:25:50 +0200

    tester litt locastorage

--- oppgaver/script.js
+++ oppgaver/script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#input')
 const button = document.querySelector('button')
+const container = document.querySelector('div')
 
 let array = [];
 let local; 
@@ -9,17 +10,18 @@ const arrayValue =  () => {
 const userInput = input.value
 array.push(userInput)
 console.log(array, 'inne i array funksjonen')
+arrayToLocal(array);
 }
 
 const arrayToLocal = (array) => {
     let setLocal = localStorage.setItem('savedArray', JSON.stringify(array))
-    localToDisplay(setLocal)
+    localToDisplay()
 }
 
-const localToDisplay = (setLocal) => {
+const localToDisplay = () => {
     let getLocal = JSON.parse(localStorage.getItem('savedArray')) || [];
     console.log(getLocal, 'inne i local funksjonen')
-
+   getLocal.forEach()
 }
 
 
