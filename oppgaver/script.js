00d015899fdc966bad020cb0340cd6734a68476b
Mohit Verma <mkv97.mkv@gmail.com>
2024-04-02 15:07:16 +0200

    c

---
+++ oppgaver/script.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+
+let array = []
+
+
+const grabValue =  () => {
+const userInput = input.value
+array.push(userInput)
+}
+
+/*
+const pushAndSave = () => {
+
+}
+*/
