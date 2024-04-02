7e32d48471d630128d9da25a9407da416fbc416c
Mohit Verma <mkv97.mkv@gmail.com>
2024-04-02 19:53:04 +0200

    c

--- oppgaver/script.js
+++ oppgaver/script.js
@@ -4,22 +4,18 @@ const fetchUrl = async () => {
     try {
         const request = await fetch(url)
         const response = await request.json()
-        console.log(response)
+        responseData(response)
         return response
     } catch (error) {
         console.error(error, 'Noe gikk galt')
     }
 }
 
-fetchUrl()
 
-const responseData = async () => {
-   const pokemonData = await fetchUrl();
-   const filteredData = pokemonData.map(pokemon =>  {
-    return {
-        name: pokemon.name,
-        url: pokemon.url
-    }
-   }) 
-    
+const responseData = async (response) => {
+   const pokemonData = response.results; 
+   console.log(pokemonData)
 }
+
+
+fetchUrl()
